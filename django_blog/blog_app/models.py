22d5be8be16d9440a9df6d5f8ff1bdb66cbf5d52
danilomirkovic <51163989+danilomirkovic@users.noreply.github.com>
2022-05-23 11:46:32 +0200

    db

--- django_blog/blog_app/models.py
+++ django_blog/blog_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = (
+    (0, 'Draft'),
+    (1, 'Published')
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, defualt=0)
+
+    def __str__(self):
+        return self.title

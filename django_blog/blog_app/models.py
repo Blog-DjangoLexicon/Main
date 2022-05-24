c27c3428dc4b978fdf869bc832d12d9195a11a53
danilomirkovic <51163989+danilomirkovic@users.noreply.github.com>
2022-05-24 10:36:16 +0200

    created Post in models.py and added user

--- django_blog/blog_app/models.py
+++ django_blog/blog_app/models.py
@@ -29,6 +29,8 @@ class Post(models.Model):
 
     title = models.CharField(max_length=200, unique=True)
 
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+
     content = models.TextField()
 
     status = models.IntegerField(choices=STATUS, default=0)

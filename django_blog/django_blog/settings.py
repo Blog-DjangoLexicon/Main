07fa1b0b4f7ba71c23c3c8bc8e9a991dbc99e7f3
danilomirkovic <51163989+danilomirkovic@users.noreply.github.com>
2022-05-29 16:29:45 +0200

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -22,7 +22,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ppmug@dy2as=@6w-$*x-i@h42+%6%_)p#=u**@btc(h-f3cabh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
